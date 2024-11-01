8d60ed06043cbb7424ba69140eaf749fb3285622
Shivank Tripathi <st.is.champion@gmail.com>
2024-11-01 23:12:07 +0530

    -

---
+++ lists_arrays/second_max.py
@@ -0,0 +1,16 @@
+def find_second_maximum(nums):
+    max = float('-inf')
+    max2 = float('-inf')
+
+    for i in nums:
+        if i > max:
+            max = i
+
+    for i in nums:
+        if i > max2 and i != max:
+            max2 = i
+    return max2
+
+
+list = [-2,-3,-5,-7]
+print(find_second_maximum(list))
